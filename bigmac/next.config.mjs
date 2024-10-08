17827846323a431452011076dcd1e83c575c7d72
Kavinder Singh <kavinder.singh.3702@gmail.com>
2024-10-08 10:11:31 +0530

    deploye vercel

--- bigmac/next.config.mjs
+++ bigmac/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "dist", // Customize the build output folder
+  // distDir: "dist", // Customize the build output folder
 };
 
 export default nextConfig;
