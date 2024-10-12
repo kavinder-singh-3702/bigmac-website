17eadc13b59d09320ac4f22141f6d7160d9f6cb5
sriparna-koar <koarsk03@gmail.com>
2024-10-12 09:40:44 +0530

    mobile view

--- bigmac/src/components/Header/Header.jsx
+++ bigmac/src/components/Header/Header.jsx
@@ -1,16 +1,15 @@
-
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Link from "next/link";
 import { ChevronDownIcon } from "@heroicons/react/solid";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Header() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen); 
 
   const handleClickOutside = (e) => {
     if (!e.target.closest(".dropdown")) {
@@ -21,7 +20,6 @@ export default function Header() {
   if (typeof window !== "undefined") {
     window.addEventListener("click", handleClickOutside);
   }
-
   return (
     <header className="bg-white dark:bg-gray-900 shadow-md fixed w-full z-10 top-0">
       <nav className="container mx-auto p-4 flex justify-between items-center">
@@ -34,30 +32,7 @@ export default function Header() {
             priority={true}
           />
         </Link>
-        <button
-          className="block lg:hidden text-gray-600 dark:text-gray-300"
-          onClick={toggleMenu}
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-        </button>
-        <ul
-          className={`${
-            isMenuOpen ? "block" : "hidden"
-          } lg:flex items-center gap-8 pr-12`}
-        >
+        <ul className="flex items-center gap-8 pr-12">
           <li>
             <Link href="/" className="hover:text-blue-300">
               Home
@@ -71,6 +46,7 @@ export default function Header() {
               Services
               <ChevronDownIcon className="w-5 h-5 ml-1" />
             </span>
+
             {isDropdownOpen && (
               <ul className="absolute bg-white shadow-lg mt-2 rounded-lg p-4 text-gray-900 w-48 dark:bg-gray-800 dark:text-gray-200">
                 <li className="py-1">
