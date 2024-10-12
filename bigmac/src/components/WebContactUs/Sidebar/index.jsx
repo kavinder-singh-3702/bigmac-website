45b7a9724790476a8a0a25197e931125860711bc
Sriparna Koar <144530081+sriparna-koar@users.noreply.github.com>
2024-10-12 11:49:26 +0530

    mobile view

--- bigmac/src/components/WebContactUs/Sidebar/index.jsx
+++ bigmac/src/components/WebContactUs/Sidebar/index.jsx
@@ -1,3 +1,4 @@
+
 import { useFormStep } from "@/hooks/use-form-steps";
 import { Step } from "./step";
 
@@ -6,20 +7,20 @@ export function Sidebar() {
 
   return (
     <div
-      className=" 
-      flex justify-center items-start z-50 bg-gray-600 pl-20  h-[172px] w-full bg-no-repeat bg-cover bg-[url('/images/bg-sidebar-mobile.svg')] 
-      sm:flex-col sm:justify-start sm:items-start sm:p-8 sm:w-[274px] sm:h-[calc(100vh-32px)] sm:bg-[url('/images/bg-sidebar-desktop.svg')] sm:rounded-lg sm:bg-center"
+      className="flex justify-center items-start z-50 bg-opacity-70 backdrop-blur-md p-6
+      bg-gradient-to-br from-gray-800 via-gray-900 to-black h-[172px] w-full 
+      bg-no-repeat bg-cover rounded-lg shadow-lg 
+      sm:flex-col sm:items-start sm:gap-8 sm:w-[280px] sm:h-[calc(100vh-32px)] 
+      sm:bg-[url('/images/bg-sidebar-desktop.svg')] sm:bg-center"
     >
-      <div className="flex flex-row gap-4 sm:flex-col sm:gap-8">
-        {steps.map((step) => {
-          return (
-            <Step
-              key={step.number}
-              step={step}
-              isActive={step.number === currentStep}
-            />
-          );
-        })}
+      <div className="flex flex-row gap-4 sm:flex-col sm:gap-8 w-full">
+        {steps.map((step) => (
+          <Step
+            key={step.number}
+            step={step}
+            isActive={step.number === currentStep}
+          />
+        ))}
       </div>
     </div>
   );
