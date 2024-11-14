3ab62dd60c4b8d37b383c8b54bf12d23df1dfac9
Kavinder Singh <kavinder.singh.3702@gmail.com>
2024-11-14 15:41:30 +0530

    hero

--- bigmac/src/app/page.jsx
+++ bigmac/src/app/page.jsx
@@ -1,14 +1,11 @@
 import { BackgroundLines } from "@/components/Background/UI/background-lines";
 import { BackgroundBeamsWithCollision } from "../components/Background/UI/Beams";
-const Hero = dynamic(() => import("@/components/Hero"), {
-  ssr: false,
-});
+import MainPage from "@/components/MainPage";
 import Services from "../components/Services";
 import { Timeline } from "@/components/Timeline/Timeline";
 import Statistics from "@/components/Statistics/Statistics";
 import { FeaturesSectionDemo } from "@/components/Background/UI/Cards";
 import ContactForm from "@/components/ContactForm/ContactForm";
-import dynamic from "next/dynamic";
 import Footer from "@/components/Footer";
 export const metadata = {
   title: "Welcome to Bigmac", // This will set the tab title
@@ -43,31 +40,7 @@ export default function Layout({ children }) {
   return (
     <div>
       <main className="mt-16 bg-black">
-        {children}
-        <section className="max-h-[100vh] bg-black flex justify-center items-center relative">
-          <BackgroundLines />
-          <div className="z-30 title absolute top-[20%] left-[4.5%] flex flex-col justify-center items-center text-center md:top-[28%]">
-            <p
-              className="text-red-300 font-extrabold text-[7vh] md:text-[8vh] leading-tight bg-clip-text text-shadow-md"
-              // style={{
-              //   backgroundImage: "url('/matCaps/golden.png')",
-              //   backgroundSize: "cover", // Use "contain" instead of "cover"
-              //   backgroundPosition: "center",
-              //   backgroundSize: "150%",   }}
-            >
-              BIGMAC AGENCY
-            </p>
-
-            <span className="block w-[90vw] md:w-[60vw] lg:w-[45vw] text-white break-words pl-2 text-center text-base md:text-xl lg:text-2xl">
-              Get the best advice from our experts, including expert developers,
-              designers, enthusiasts, and managers.
-            </span>
-          </div>
-          <div className="h-screen absolute w-screen lg:translate-x-[50%] md:ml-20 z-0 lg:w-1/2">
-            <Hero />
-          </div>
-        </section>
-
+        <MainPage />
         <Services />
         <BackgroundBeamsWithCollision>
           <Timeline data={ProcessData} className={"mt-20"} />
@@ -75,7 +48,7 @@ export default function Layout({ children }) {
         <Statistics />
         <FeaturesSectionDemo />
         <ContactForm />
-         <Footer />
+        <Footer />
       </main>
     </div>
   );
